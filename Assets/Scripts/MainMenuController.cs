12625243ae9174fd81b4bbb5fc25b112c5deb040
Raine Koizumi <rainekoizumi@berkeley.edu>
2023-09-13 14:49:36 -0700

    finish

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text m_HighScore;
+
+    private string m_DefaultHighScoreText;
+
+    #region Initialization
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        m_DefaultHighScoreText = m_HighScore.text;
+    }
+    private void Start()
+    {
+        UpdateHighScore();
+    }
+    #endregion
+
+    public void PlayArena()
+    {
+        SceneManager.LoadScene("Arena");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void UpdateHighScore()
+    {
+        if (PlayerPrefs.HasKey("HS"))
+        {
+            m_HighScore.text = m_DefaultHighScoreText.Replace("%S", PlayerPrefs.GetInt("HS").ToString());
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HS", 0);
+            m_HighScore.text = m_DefaultHighScoreText.Replace("%S", "0");
+        }
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("HS", 0);
+        UpdateHighScore();
+    }
+}
---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("hi")]
+    private Transform m_PlayerTransform;
+
+    [SerializeField]
+    [Tooltip("asdf")]
+    private Vector3 m_Offset;
+
+    [SerializeField]
+    [Tooltip("asdf")]
+    private float m_RotationSpeed = 10;
+    #endregion
+
+    #region Main Updates
+    private void LateUpdate()
+    {
+        Vector3 newPos = m_PlayerTransform.position + m_Offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, 1);
+
+        float rotationAmount = m_RotationSpeed * Input.GetAxis("Mouse X");
+        transform.RotateAround(m_PlayerTransform.position, Vector3.up, rotationAmount);
+
+        m_Offset = transform.position - m_PlayerTransform.position;
+    }
+    #endregion
+}
